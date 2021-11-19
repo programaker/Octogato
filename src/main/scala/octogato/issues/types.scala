28508fa7f03420a90b74a8caa77ce18d83be0100
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-19 15:36:59 +0100

    Fix Color refinement

--- src/main/scala/octogato/issues/types.scala
+++ src/main/scala/octogato/issues/types.scala
@@ -3,7 +3,7 @@ package octogato.issues
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.Interval
 import eu.timepit.refined.numeric.Positive
-import eu.timepit.refined.collection.Size
+import eu.timepit.refined.collection.{MaxSize, MinSize, Size}
 import eu.timepit.refined.boolean.And
 import eu.timepit.refined.string.HexStringSpec
 
@@ -13,5 +13,6 @@ type Page = Int Refined PageP
 type PerPageP = Interval.Closed[1, 100]
 type PerPage = Int Refined PerPageP
 
-type ColorP = Size[6] And HexStringSpec
+type ColoSize = 6
+type ColorP = MinSize[ColoSize] And MaxSize[ColoSize] And HexStringSpec
 type Color = String Refined ColorP
