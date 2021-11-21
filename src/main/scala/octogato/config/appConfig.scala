b665cc765b9f2bce275079e0abd80eadb0dfc647
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-21 13:10:51 +0100

    Changes in config

--- src/main/scala/octogato/config/appConfig.scala
+++ src/main/scala/octogato/config/appConfig.scala
@@ -4,8 +4,12 @@ import octogato.common.UriString
 import octogato.common.Token
 
 case class AppConfig(
-  apiUri: UriString,
+  api: ApiConfig,
   authorization: AuthorizationConfig
+)
+
+case class ApiConfig(
+  apiHost: UriString
 )
 
 case class AuthorizationConfig(
--- src/main/scala/octogato/config/givens.scala
+++ src/main/scala/octogato/config/givens.scala
@@ -7,6 +7,9 @@ import octogato.common.refineE
 import pureconfig.ConfigReader
 import pureconfig.error.ExceptionThrown
 import pureconfig.error.FailureReason
+import pureconfig.generic.derivation.default.*
 
 given [T, P](using cr: ConfigReader[T], v: Validate[T, P]): ConfigReader[Refined[T, P]] =
   cr.emap(refineE[P](_).leftMap(ExceptionThrown.apply))
+
+given ConfigReader[AppConfig] = ConfigReader.derived
