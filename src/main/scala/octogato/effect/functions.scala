902e9f534526a2a3786b953b2fd12e76544a2767
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-23 18:03:08 +0100

    Rename

--- src/main/scala/octogato/effect/functions.scala
+++ src/main/scala/octogato/effect/functions.scala
@@ -6,7 +6,7 @@ import cats.effect.kernel.MonadCancel
 object syntax:
   extension [F[_], A](res: Resource[F, A])(using MonadCancel[F, Throwable])
     /** Use a resource as a given through a Context Function */
-    def useAsGiven[B](f: A ?=> F[B]): F[B] = res.use { a =>
+    def useGiven[B](f: A ?=> F[B]): F[B] = res.use { a =>
       given A = a
       f
     }
