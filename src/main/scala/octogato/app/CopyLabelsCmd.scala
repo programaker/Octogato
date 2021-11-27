0002fd7d874353a5c86df8c6ad6b34043fd4cee4
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-27 21:46:53 +0100

    Reorganizing some code

---
+++ src/main/scala/octogato/app/CopyLabelsCmd.scala
@@ -0,0 +1,33 @@
+package octogato.app
+
+import cats.syntax.apply.*
+import octogato.common.Token
+import octogato.label.LabelPath
+import com.monovore.decline.Opts
+import com.monovore.decline.refined.*
+
+// $ octogato copy-labels --from 'programaker/Joguin2' --to 'programaker/Spotification' --token <optional>
+case class CopyLabelsCmd(from: LabelPath, to: LabelPath, token: Option[Token])
+object CopyLabelsCmd:
+  val opts: Opts[CopyLabelsCmd] =
+    Opts.subcommand("copy-labels", "Copy issue labels from one repository to another") {
+      (
+        Opts.option[LabelPath](
+          "from",
+          "Origin repository in the form 'owner/repo'",
+          metavar = "owner/repo"
+        ),
+        Opts.option[LabelPath](
+          "to",
+          "Target repository in the form 'owner/repo'",
+          metavar = "owner/repo"
+        ),
+        Opts
+          .option[Token](
+            "token",
+            "[optional] Github personal token. Can also be provided through a GITHUB_TOKEN env variable",
+            metavar = "owner/repo"
+          )
+          .orNone
+      ).mapN(CopyLabelsCmd.apply)
+    }
--- src/main/scala/octogato/app/OctogatoCLIApp.scala
+++ src/main/scala/octogato/app/OctogatoCLIApp.scala
@@ -1,14 +1,10 @@
 package octogato.app
 
+import cats.syntax.apply.*
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.std.Console
-import cats.syntax.apply.*
-import cats.syntax.applicative.*
-import cats.syntax.show.*
-import cats.syntax.foldable.*
 import com.monovore.decline.Opts
-import com.monovore.decline.refined.*
 import octogato.common.Token
 import octogato.common.given
 import octogato.label.LabelPath
@@ -18,7 +14,6 @@ import com.monovore.decline.effect.CommandIOApp
 import octogato.label.LabelService
 import octogato.config.ConfigService
 import octogato.common.http.HttpClientBackend
-import octogato.effect.syntax.*
 import cats.effect.kernel.Resource
 import octogato.label.CopyLabelsResult
 import octogato.label.CopyLabelsError
@@ -32,9 +27,8 @@ object OctogatoCLIApp extends CommandIOApp(name = "octogato", header = ""):
     val reportSuccess = console.println(_: CopyLabelsResult).map(_ => ExitCode.Success)
     val reportError = console.errorln(_: CopyLabelsError).map(_ => ExitCode.Error)
 
-    HttpClientBackend
-      .makeResource[IO]
-      .both(Log.makeResource[IO])
+    (HttpClientBackend.makeResource[IO], Log.makeResource[IO])
+      .tupled
       .use { (httpClient, log) =>
         given HttpClientBackend[IO] = httpClient
         given Log[IO] = log
@@ -49,29 +43,3 @@ object OctogatoCLIApp extends CommandIOApp(name = "octogato", header = ""):
         yield res
       }
       .flatMap(_.fold(reportError, reportSuccess))
-
-// $ octogato copy-labels --from 'programaker/Joguin2' --to 'programaker/Spotification' --token <optional>
-case class CopyLabelsCmd(from: LabelPath, to: LabelPath, token: Option[Token])
-object CopyLabelsCmd:
-  val opts: Opts[CopyLabelsCmd] =
-    Opts.subcommand("copy-labels", "Copy issue labels from one repository to another") {
-      (
-        Opts.option[LabelPath](
-          "from",
-          "Origin repository in the form 'owner/repo'",
-          metavar = "owner/repo"
-        ),
-        Opts.option[LabelPath](
-          "to",
-          "Target repository in the form 'owner/repo'",
-          metavar = "owner/repo"
-        ),
-        Opts
-          .option[Token](
-            "token",
-            "[optional] Github personal token. Can also be provided through a GITHUB_TOKEN env variable",
-            metavar = "owner/repo"
-          )
-          .orNone
-      ).mapN(CopyLabelsCmd.apply)
-    }
