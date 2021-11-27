1174dad69b3acb63ac34ee5a4ce0c235eaff1fc4
programaker <programaker@gmail.com>
2021-11-27 08:22:38 +0100

    Main class draft

---
+++ src/main/scala/octogato/cliapp/OctogatoCLIApp.scala
@@ -0,0 +1,8 @@
+package octogato.cliapp
+
+import cats.effect.IOApp
+import cats.effect.IO
+import cats.effect.ExitCode
+
+object OctogatoCLIApp extends IOApp:
+  override def run(args: List[String]): IO[ExitCode] = ???
