c03aedb953a60054a2ded2d683d1a2c5af1b1689
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-29 20:00:24 +0100

    Was using 's' instead of 'show' interpolator

--- src/main/scala/octogato/label/CopyLabelsError.scala
+++ src/main/scala/octogato/label/CopyLabelsError.scala
@@ -1,9 +1,10 @@
 package octogato.label
 
 import cats.Show
+import cats.syntax.show.*
 
 case class CopyLabelsError(error: Throwable, from: LabelPath, to: LabelPath) extends Exception(error)
 object CopyLabelsError:
-  given Show[CopyLabelsError] = Show.show { e =>
-    s"Error copying labels from ${e.from} to ${e.to}: ${e.getMessage}"
+  given (using Show[LabelPath]): Show[CopyLabelsError] = Show.show { e =>
+    show"Error copying labels from ${e.from} to ${e.to}: ${e.getMessage}"
   }
